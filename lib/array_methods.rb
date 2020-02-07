b3e81eae4c6aa59a34aed8e3a608f171d3ab2c26
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-07 02:27:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,16 +1,8 @@
 def find_element_index(array, value_to_find)
 
-counter = 0
+while array.include?(value_to_find) == true do
 
-while counter < array.length do
-
-counter += 1
-
-while value_to_find == element do
-
-  puts array(index)
-
-end
+puts array.index
 
 end
 
